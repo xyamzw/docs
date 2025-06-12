bda0acd9809c8aeb300c531dfb858ccf90529f9a
未觉寻星 <2937150394@qq.com>
2025-06-13 05:31:14 +0800

    fix: repair pricing page to url

--- docs/.vuepress/components/api/index.ts
+++ docs/.vuepress/components/api/index.ts
@@ -1,7 +1,5 @@
 export const apis: Record<string, string> = {
-  cf: "https://api.nn.ci",  // max 10, minutes 60, need further investigation
-  cn: "https://api.xhofe.top",
-  de: "http://api-my-api-5hw5ou-b810d8-80-75-218-107.traefik.me"
+  cf: "https://oplist.8mi.tech/docs",  // max 10, minutes 60, need further investigation
 };
 
 if (typeof location !== "undefined") {
