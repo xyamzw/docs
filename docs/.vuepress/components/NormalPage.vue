baf5720e4040e525a73882cb7ceed9885cc1befa
Yinan Qin <39023210+elysia-best@users.noreply.github.com>
2025-06-13 16:17:26 +0800

    fix: remove ads (#19)

--- docs/.vuepress/components/NormalPage.vue
+++ docs/.vuepress/components/NormalPage.vue
@@ -1,24 +1,10 @@
 <template>
   <NormalPage>
     <template #contentBefore>
-      <div class="theme-hope-content ads-container" vp-content>
-        <div class="mingdao" v-if="showMingdao">
-          <a href="https://www.mingdao.com?s=utm_51=utm_source=alist&utm_medium=banner&utm_campaign=%E5%93%81%E7%89%8C%E6%8E%A8%E5%B9%BF&utm_content=IT%E8%B5%8B%E8%83%BD%E4%B8%9A%E5%8A%A1"
-            target="_blank">
-            <img src="/img/ss/mingdao-h.png" alt="" />
-            <span>{{ spStr }}</span>
-          </a>
-        </div>
-        <div class="wwads wwads-cn wwads-horizontal" data-id="213" style="width:100% !important" v-else>
-        </div>
-        <ApiSelect v-if="isTool" />
-      </div>
+
     </template>
     <template #contentAfter>
-      <div class="bottom-wwads">
-        <div class="wwads wwads-cn wwads-horizontal" data-id="213" style="width:90% !important" v-if="showMingdao">
-        </div>
-      </div>
+
     </template>
   </NormalPage>
 </template>
@@ -73,16 +59,5 @@ const spStr = computed(() => {
       bottom: 10px;
     }
   }
-}
-
-.bottom-wwads {
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-}
-
-.ads-container {
-  padding-top: 0 !important;
-  padding-bottom: 0 !important;
 }
 </style>
